22a52e498dcd4d52a0139f8ddcb23466e913b9c0
leodido <leodidonato@gmail.com>
2014-05-01 17:30:40 +0200

    Packaging

---
+++ component.js
@@ -0,0 +1,17 @@
+{
+  "name": "h5validate",
+  "repo": "components/h5validate",
+  "version": "0.9.0",
+  "description": "h5validate component",
+  "keywords": [
+    "jquery",
+    "validation",
+    "h5validate",
+    "component"
+  ],
+  "main": "jquery.h5validate.js",
+  "scripts": [
+    "jquery.h5validate.js"
+  ],
+  "license": "MIT"
+}
